9336346994436fda5a73dd8f1ce01ddebd2715a3
John Green <61171945+johgreen@users.noreply.github.com>
2022-08-15 22:27:04 -0400

    Update pymongo_examples.py

--- pymongo_examples.py
+++ pymongo_examples.py
@@ -1,5 +1,5 @@
 import pymongo
-import pprint #pretty print
+import pprint 
 
 
 uri = "mongodb://127.0.0.1:27017/"
@@ -8,8 +8,6 @@ client = pymongo.MongoClient(uri) #Connect to a local MongoDB server
 
 db = client.movieData #Access the "movieData" database
 movies = db.movies #Access the "movies" collection
-print(movies.find_one()) #print a document
-
 
 for docs in movies.find({"runtime" : {"$gt" : 60}}): #example query
     pprint.pprint(docs)
@@ -25,7 +23,6 @@ pipeline = [
     {"$limit" : 30}
 ]
 query1 = list(movies.aggregate(pipeline))
-pprint.pprint(query1)
 
 #find all movies with "Drama" and "Action" in the genre array
 
@@ -33,14 +30,29 @@ pipeline2 = [
     {"$match" : {"genres" : {"$all" : ["Drama" , "Action"]}}}
 ]
 query2 = list(movies.aggregate(pipeline2))
-pprint.pprint(query2)
 
 #######################
 
 #explain feature examples
 explain_query = db.command('aggregate' , 'movieData' , pipeline = pipeline , explain = True)
-pprint.pprint(explain_query)
-
-
 explain_query2 = movies.find({"name" : "Last Resort"} , {"name" : 1 , "rating" : 1}).explain()['executionStats']
-pprint.pprint(explain_query2)
+
+
+if __name__ == "__main__":
+    print(movies.find_one())
+    pprint.pprint(query1)
+    pprint.pprint(query2)
+    pprint.pprint(explain_query)
+    pprint.pprint(explain_query2)
+
+
+
+
+
+
+
+
+
+
+
+
